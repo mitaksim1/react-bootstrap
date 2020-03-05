0ce0a30f556e66de31462ed9057fe771dbce50f1
mitaksim1 <miriamt.simonnet@gmail.com>
2020-03-05 15:46:33 +0100

    Navigation bar's creation with bootstrap

--- src/App.js
+++ src/App.js
@@ -5,23 +5,24 @@ import { About } from './About';
 import { Contact } from './Contact';
 import { NoMatch } from './NoMatch';
 import  { Layout } from './components/Layout';
+import { NavigationBar } from './components/NavigationBar';
 
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-      <Layout>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
-            <Route component={NoMatch} />
-          </Switch>
-        </Router>  
-      </Layout>
-           
+        <NavigationBar />
+          <Layout>
+            <Router>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/contact" component={Contact} />
+                <Route component={NoMatch} />
+              </Switch>
+            </Router>  
+          </Layout>    
       </React.Fragment>
     );
   }
